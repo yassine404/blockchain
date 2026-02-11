4a56a4c14781cac35ca9901b89a7d46d76c69847
ymoun036 <ymoun036@uottawa.ca>
2026-02-11 11:17:15 +0100

    block v1

---
+++ block.js
@@ -0,0 +1,10 @@
+class block{
+ 
+    constructor(index, previousHash, timetamp, data, hash){
+        this.index=index;
+        this.previousHash=previousHash;
+        this.timetamp=timetamp;
+        this.data=data;
+        this.hash=hash;
+    }
+}
